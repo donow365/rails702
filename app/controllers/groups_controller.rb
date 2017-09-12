e87a001e85ff2c44a8c516dd43a5e3e1c50b2eaa
donow365 <donow365@gmail.com>
2017-09-12 14:47:30 +0800

    join&quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :quit, :join]
 before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
@@ -47,6 +47,31 @@ def destroy
     flash[:alert] = "group deleted"
   redirect_to groups_path
 
+end
+
+def join
+  @group = Group.find(params[:id])
+
+  if !current_user.is_member_of?(@group)
+    current_user.join!(@group)
+    flash[:notice] = "你已经是本讨论版成员了"
+  end
+
+  redirect_to group_path(@group)
+
+end
+
+def quit
+  @group = Group.find(params[:id])
+
+  if current_user.is_member_of?(@group)
+    current_user.quit!(@group)
+    flash[:alert] = "你已经退出本讨论版！"
+  else
+    flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+  end
+
+  redirect_to group_path(@group)
 end
 
 private
